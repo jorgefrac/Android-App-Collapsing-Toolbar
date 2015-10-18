3833207b90834e54ccd0d56e07a188bba0efb273
jorge_frac <jorge_frac@hotmail.com>
2015-10-18 00:14:56 -0500

    Add resources drawable, colors, strings, styles, dimen

--- app/src/main/java/com/example/jorge/android_app_collapsing_toolbar/MainActivity.java
+++ app/src/main/java/com/example/jorge/android_app_collapsing_toolbar/MainActivity.java
@@ -1,37 +1,69 @@
 package com.example.jorge.android_app_collapsing_toolbar;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CollapsingToolbarLayout collapser;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getToolbar();
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        collapser = (CollapsingToolbarLayout) findViewById(R.id.collapser);
+        collapser.setTitle("Collapsing");
+    }
+
+    private void getToolbar() {
+        // Añadir la Toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void showSnackBar(String msg) {
+        Snackbar
+                .make(findViewById(R.id.coordinator), msg, Snackbar.LENGTH_LONG)
+                .show();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                showSnackBar("Se abren los ajustes");
+                return true;
+            case R.id.action_add:
+                showSnackBar("Añadir a contactos");
+                return true;
+            case R.id.action_favorite:
+                showSnackBar("Añadir a favoritos");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
